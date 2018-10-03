43886bfd05f496b4b1e22526e334ad8e4e5380e8
Filipi <filipi@ciandt.com>
2018-10-03 13:11:15 -0300

    Development of app

---
+++ MemeMeFilipi/MemeMeFilipi/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMeFilipi
+//
+//  Created by Filipi Brentegani on 03/10/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memedImage: UIImage
+}
--- MemeMeFilipi/MemeMeFilipi/ViewController.swift
+++ MemeMeFilipi/MemeMeFilipi/ViewController.swift
@@ -6,20 +6,185 @@
 //  Copyright © 2018 Udacity. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    private var isCameraAvailable: Bool = UIImagePickerController.isSourceTypeAvailable(.camera)
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setInitialStateScreen()
+        
+        topTextField.delegate = self
+        bottomTextField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        subscribeToKeyboardNotifications()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            cameraButton.isEnabled = isCameraAvailable
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    self.cameraButton.isEnabled = self.isCameraAvailable
+                }
+            }
+        case .denied:
+            return
+        case .restricted:
+            return
+        }
+    }
+    
+    func save() {
+        let meme = Meme(topText: topTextField.text!, bottomText: bottomTextField.text!, originalImage: imageView.image!, memedImage: generateMemedImage())
+        
+        //TODO share the meme
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide toolbar and navigationbar
+    
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // TODO: Show toolbar and navigationbar
+        
+        return memedImage
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func onAlbumButton(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func onActionButton(_ sender: Any) {
+    }
+    
+    @IBAction func onCancelButton(_ sender: Any) {
+        setInitialStateScreen()
+    }
+    
+    private func setInitialStateScreen() {
+        topTextField.isEnabled = false
+        topTextField.text = ""
+        bottomTextField.isEnabled = false
+        bottomTextField.text = ""
+        imageView.image = nil
+        cancelButton.isEnabled = false
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if bottomTextField.isFirstResponder {
+            view.frame.origin.y = getKeyboardHeight(notification) * -1
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        if bottomTextField.isFirstResponder {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
 }
 
+extension ViewController : UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        guard let text = textField.text else { return false }
+        if textField == topTextField && text == "TOP" {
+            topTextField.text = ""
+        } else if textField == bottomTextField && text == "BOTTOM" {
+            bottomTextField.text = ""
+        }
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        guard let text = textField.text else { return false }
+        if textField == topTextField && text == "" {
+            topTextField.text = "TOP"
+        } else if textField == bottomTextField && text == "" {
+            bottomTextField.text = "BOTTOM"
+        }
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == topTextField {
+            topTextField.endEditing(true)
+        } else if textField == bottomTextField {
+            bottomTextField.endEditing(true)
+        }
+        return true
+    }
+}
+
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            topTextField.text = "TOP"
+            bottomTextField.text = "BOTTOM"
+            topTextField.isEnabled = true
+            bottomTextField.isEnabled = true
+            cancelButton.isEnabled = true
+        }
+        dismiss(animated:true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
